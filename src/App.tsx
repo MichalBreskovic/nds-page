61bae53ac6fbc595aca7dcefa897355d0ec0415d
Michal Breškovič <michal.breskovic@gmail.com>
2022-03-30 16:20:15 +0200

    feat: added default route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
     BrowserRouter,
     Routes,
     Route,
+    Navigate,
 } from "react-router-dom";
 import Main from "./components/main";
 import TermsOfConditions from "./components/terms-of-conditions";
@@ -15,10 +16,11 @@ function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/terms-and-conditions" element={<TermsOfConditions />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="/copyright" element={<Copyright />} />
+              <Route path="" element={<Main />} />
+              <Route path="terms-and-conditions" element={<TermsOfConditions />} />
+              <Route path="privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="copyright" element={<Copyright />} />
+              <Route path="*" element={<Navigate to={"/"} />} />
           </Routes>
       </BrowserRouter>
   );
